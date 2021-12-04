bacd0995a42cc2017a9c44715e649c0a738d5a1d
betogroo <luizhumberto@gmail.com>
2021-12-04 16:03:06 -0300

    chore: add sass

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,30 @@
 <script setup lang="ts"></script>
 
 <template>
+  <div class="nav-bar">
+    <div class="nav">
+      <router-link :to="{ name: 'Home' }">Home</router-link> |
+      <router-link :to="{ name: 'About' }">About</router-link>
+    </div>
+  </div>
   <router-view />
 </template>
-
-<style></style>
+<style lang="scss" scoped>
+* {
+  font-family: Roboto, Helvetica, Arial, sans-serif;
+  margin: 0;
+  padding: 0;
+}
+.nav-bar {
+  display: flex;
+  justify-content: center;
+}
+.nav {
+  a {
+    color: red;
+  }
+  a:hover {
+    color: blue;
+  }
+}
+</style>
