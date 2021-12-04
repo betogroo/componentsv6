a2bea33c5bd2f28907278206b0e20510965eaead
betogroo <luizhumberto@gmail.com>
2021-12-04 15:36:34 -0300

    feat: add Avout view

--- src/modules/home/routes.ts
+++ src/modules/home/routes.ts
@@ -1,11 +1,17 @@
 import { RouteRecordRaw } from 'vue-router'
 import { HomeView } from './views'
+import { AboutView } from './views'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'Home',
     component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: AboutView
   }
 ]
 
