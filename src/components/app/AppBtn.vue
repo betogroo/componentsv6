7335eac4f864ac12c075feedba65c9d61f9b30d0
betogroo <luizhumberto@gmail.com>
2021-12-04 19:54:01 -0300

    feat: add pwa

---
+++ src/components/app/AppBtn.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+interface Props {
+  text?: string
+  type?: string
+}
+// eslint-disable-next-line no-undef
+const props = withDefaults(defineProps<Props>(), {
+  text: 'Click',
+  type: 'button'
+})
+</script>
+
+<template>
+  <div>
+    <button>{{ props.text }}</button>
+  </div>
+</template>
+
+<style></style>
